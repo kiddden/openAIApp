5efa652edc78be85f9ed3f7e055963de54ad4aa7
kiddden <kazzer@knu.ua>
2023-02-09 16:39:50 +0200

    Initial Commit

---
+++ openAIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openAIApp
+//
+//  Created by Eugene Ned on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
