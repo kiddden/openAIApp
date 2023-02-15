7d7f1808baa4cf0fd3fdb1b0076846757c4b3b5c
kiddden <kazzer@knu.ua>
2023-02-15 20:58:13 +0200

    moving the chat view up when keyboard appears

--- openAIApp/Screens/ChatVC.swift
+++ openAIApp/Screens/ChatVC.swift
@@ -9,10 +9,13 @@ import UIKit
 import SwiftUI
 
 class ChatVC: UIViewController {
-
+    
+    private var keyboardHeight: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         congifureVC()
+        setupObserversForKeyboard()
     }
     
     private func congifureVC() {
@@ -25,5 +28,26 @@ class ChatVC: UIViewController {
         view.addSubview(messagesView.view)
         messagesView.view.frame = view.bounds
         messagesView.didMove(toParent: self)
+    }
+    
+    private func setupObserversForKeyboard() {
+        // Registering observers for keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        // Getting the height of the keyboard
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            keyboardHeight = keyboardFrame.height
+        }
+        
+        // Moving view up when keyboard appears
+        UIView.animate(withDuration: 0.3) { self.view.frame.origin.y = -self.keyboardHeight+10 }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        // Discarding the previus view position when keyboard dissapears
+        UIView.animate(withDuration: 0.3) { self.view.frame.origin.y = 0 }
     }
 }
